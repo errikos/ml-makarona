8445d834f7f64218805c78613c62fdb0b8922f64
Ivi Dimopoulou <ivi.dimopoulou@epfl.ch>
2018-10-12 18:13:16 +0200

    Add loaders, parsers and testers

---
+++ project1/src/util/loaders.py
@@ -0,0 +1,85 @@
+import numpy as np
+import csv
+
+# def load_data():
+#     """Load data."""
+#     data = np.loadtxt("dataEx3.csv", delimiter=",", skiprows=1, unpack=True)
+#     x = data[0]
+#     y = data[1]
+#     return x, y
+
+
+# def load_data_from_ex02(sub_sample=True, add_outlier=False):
+#     """Load data and convert it to the metric system."""
+#     path_dataset = "height_weight_genders.csv"
+#     data = np.genfromtxt(
+#         path_dataset, delimiter=",", skip_header=1, usecols=[1, 2])
+#     height = data[:, 0]
+#     weight = data[:, 1]
+#     gender = np.genfromtxt(
+#         path_dataset, delimiter=",", skip_header=1, usecols=[0],
+#         converters={0: lambda x: 0 if b"Male" in x else 1})
+#     # Convert to metric system
+#     height *= 0.025
+#     weight *= 0.454
+
+#     # sub-sample
+#     if sub_sample:
+#         height = height[::50]
+#         weight = weight[::50]
+
+#     if add_outlier:
+#         # outlier experiment
+#         height = np.concatenate([height, [1.1, 1.2]])
+#         weight = np.concatenate([weight, [51.5/0.454, 55.3/0.454]])
+
+#     return height, weight, gender
+
+
+def load_csv_data(data_path, sub_sample=False, is_logistic=False):
+    """Load data and return y (class labels), tX (features) and ids.
+    If the data is to be used by a logistic method, the labels should be (0,1).
+    """
+    y = np.genfromtxt(data_path, delimiter=",", skip_header=1, dtype=str, usecols=1)
+    x = np.genfromtxt(data_path, delimiter=",", skip_header=1)
+    ids = x[:, 0].astype(np.int)
+    input_data = x[:, 2:]
+
+    # convert class labels from strings to binary (-1,1) or (0,1) if logistic
+    yb = np.ones(len(y))
+    if (is_logistic):
+        yb[np.where(y == 'b')] = 0
+    else:
+        yb[np.where(y == 'b')] = -1
+
+    # sub-sample
+    if sub_sample:
+        yb = yb[0:100]
+        input_data = input_data[0:100, :]
+        ids = ids[0:100]
+
+    return yb, input_data, ids
+
+# dark code
+def build_model_data(height, weight):
+    """Form (y,tX) to get regression data in matrix form."""
+    print("Build model data is actually called")
+    y = weight
+    x = height
+    num_samples = len(y)
+    tx = np.c_[np.ones(num_samples), x]
+    return y, tx
+    
+
+def create_csv_submission(ids, y_pred, name):
+    """Create an output file in csv format for submission to kaggle.
+    Arguments: ids (event ids associated with each prediction)
+               y_pred (predicted class labels)
+               name (string name of .csv output file to be created)
+    """
+    with open(name, 'w') as csvfile:
+        fieldnames = ['Id', 'Prediction']
+        writer = csv.DictWriter(csvfile, delimiter=",", fieldnames=fieldnames, lineterminator = '\n')
+        writer.writeheader()
+        for r1, r2 in zip(ids, y_pred):
+            writer.writerow({'Id': int(r1), 'Prediction': int(r2)})
---
+++ project1/src/util/parsers.py
@@ -0,0 +1,165 @@
+import numpy as np
+
+def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
+    """
+    Generate a minibatch iterator for a dataset.
+    Takes as input two iterables (here the output desired values 'y' and the input data 'tx')
+    Outputs an iterator which gives mini-batches of `batch_size` matching elements from `y` and `tx`.
+    Data can be randomly shuffled to avoid ordering in the original data messing with the randomness of the minibatches.
+    Example of use :
+    for minibatch_y, minibatch_tx in batch_iter(y, tx, 32):
+        <DO-SOMETHING>
+    """
+    data_size = len(y)
+
+    if shuffle:
+        shuffle_indices = np.random.permutation(np.arange(data_size))
+        shuffled_y = y[shuffle_indices]
+        shuffled_tx = tx[shuffle_indices]
+    else:
+        shuffled_y = y
+        shuffled_tx = tx
+    for batch_num in range(num_batches):
+        start_index = batch_num * batch_size
+        end_index = min((batch_num + 1) * batch_size, data_size)
+        if start_index != end_index:
+            yield shuffled_y[start_index:end_index], shuffled_tx[start_index:end_index]
+
+
+def build_poly(x, degree, do_add_bias):
+    """Polynomial basis functions for input data x, for j=0 up to j=degree.
+    Important: Adds bias column.
+    """
+    numOfFeat = x.shape[1]
+    richArray = np.zeros((x.shape[0], degree * numOfFeat))
+    j = 0
+
+    for feat in range(0, numOfFeat):
+
+        for i in range(1, degree + 1):
+            richArray[:, j] = np.power(x[:, feat], i)
+            j = j + 1
+
+    if(do_add_bias):
+        zeroethPower = np.ones((x.shape[0], 1))
+        res = np.concatenate((zeroethPower, richArray), axis=1)
+    else:
+        res = richArray
+    return res
+
+
+def split_data(x, y, ids, ratio, seed=1):
+    """Split data determninistically."""
+    np.random.seed(seed)
+
+    train_x = x[0:np.floor(ratio * len(x)).astype(int)]
+    train_y = y[0:np.floor(ratio * len(x)).astype(int)]
+    train_ids = y[0:np.floor(ratio * len(x)).astype(int)]
+
+    test_x = x[np.floor(ratio * len(x)).astype(int):len(x)]
+    test_y = y[np.floor(ratio * len(x)).astype(int):len(x)]
+    test_ids = y[np.floor(ratio * len(x)).astype(int):len(x)]
+
+    return train_x, train_y, train_ids, test_x, test_y, test_ids
+
+
+def split_data_rand(x, y, ids, ratio, seed=1):
+    """Split data to training and testing set given the ratio."""
+    # print(x[1,:])
+    np.random.seed(seed)
+    y = y.reshape(y.shape[0], 1)
+    allData = np.concatenate((y, x), axis=1)
+
+    np.random.shuffle(allData)
+
+    y = allData[:, 0]
+    x = allData[:, 1:]
+
+    # print(x[1,:])
+    train_x = x[0:np.floor(ratio * len(x)).astype(int)]
+    train_y = y[0:np.floor(ratio * len(x)).astype(int)]
+    train_ids = y[0:np.floor(ratio * len(x)).astype(int)]
+
+    test_x = x[np.floor(ratio * len(x)).astype(int):len(x)]
+    test_y = y[np.floor(ratio * len(x)).astype(int):len(x)]
+    test_ids = y[np.floor(ratio * len(x)).astype(int):len(x)]
+
+    return train_x, train_y, train_ids, test_x, test_y, test_ids
+
+
+def standardize(x):
+    """Standardise each feature. Returns mean of last feat."""
+
+    for i  in range(x.shape[1]):
+
+        feature = x[:,i]
+        invalid = [feature == -999.0]
+        valid   = [feature != -999.0]
+
+        mean    = np.mean(feature[valid])
+        std     = np.std(feature[valid])
+
+        feature = (feature-mean)/std
+
+        feature[invalid] = 0
+        x[:,i] = feature
+
+    return x, mean, std
+
+
+# def standardize(x):
+#     """Standardize the original data set."""
+#     mean_x = np.mean(x)
+#     # print("MEANS ARE: ", mean_x)
+#     x = x - mean_x
+#     std_x = np.std(x)
+#     # print("STDS ARE: ", std_x)
+#     x = x / std_x
+#     return x, mean_x, std_x
+
+
+def sigmoid(z):
+    """Return the sigmoid of x.
+    x can be scalar or vector
+    could remove one exp
+    """
+    sig = np.exp(z)/(np.exp(z) + 1)
+    # print("Sigmoid is: ", sig, " (if high, causes overflow)")
+    return sig
+
+
+def predict_labels(weights, data, is_logistic):
+    """Generate class predictions given weights, and a test data matrix."""
+    if(is_logistic):
+        y_pred = np.dot(data, weights)
+        #y_pred = sigmoid(np.dot(data, weights))
+        y_pred[np.where(y_pred <= 0.5)] = 0
+        y_pred[np.where(y_pred > 0.5)] = 1
+    else:
+        y_pred = np.dot(data, weights)
+        y_pred[np.where(y_pred <= 0)] = -1
+        y_pred[np.where(y_pred > 0)] = 1
+
+    return y_pred
+
+
+def cut_samples(some_y, some_tx):
+    """Remove all samples that have at least one -999 value."""
+    some_y = some_y.reshape(some_y.shape[0], 1)
+    allData = np.concatenate((some_y, some_tx), axis=1)
+    badRows, badCols = np.where(allData == -999)
+    uniqueBadRows = np.unique(badRows)
+
+    allData = np.delete(allData, uniqueBadRows, 0)
+    cut_y = allData[:, 0]
+    cut_tx = allData[:, 1:]
+    return cut_y, cut_tx
+
+
+def cut_features(some_tx):
+    """Remove features that have at least on -999 value."""
+    return np.delete(some_tx, [0, 4, 5, 6, 12, 23, 24, 25, 26, 27, 28], 1)
+    # return np.delete(some_tx, [4,5,6,12,23,24,25,26,27,28], 1)
+    # return np.delete(some_tx, [4,5,6,12,26,27,28], 1)
+    # return np.delete(some_tx, [4], 1)
+    # return some_tx
