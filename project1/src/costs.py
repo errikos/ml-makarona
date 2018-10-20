f8edb3e0a4a90348863921a486a128e26405fbbd
Ergys Dona <errikosd@gmail.com>
2018-10-20 16:37:22 +0200

    Add GD and SGD

--- project1/src/costs.py
+++ project1/src/costs.py
@@ -5,7 +5,17 @@ def mse(e):
     return 1/2 * np.mean(e**2)
 
 
+def mae(e):
+    return np.mean(np.abs(e))
+
+
 def compute_mse(y, tx, w):
-    """compute the loss by mse."""
+    """Compute the loss by MSE (Mean Square Error)."""
     e = y - tx.dot(w)
     return mse(e)
+
+
+def compute_mae(y, tx, w):
+    """Compute the loss by MAE (Mean Absolute Error)."""
+    e = y - tx.dot(w)
+    return mae(e)
--- project1/src/implementations.py
+++ project1/src/implementations.py
@@ -4,10 +4,11 @@ import numpy as np
 
 import costs
 import gradients
+import helpers
 
 
 def least_squares_GD(y, tx, initial_w, max_iters, gamma):
-    # Define parameters to store w and loss
+    """Linear regression usign Gradient Descent."""
     w = initial_w
     for n_iter in range(max_iters):
         # compute loss, gradient
@@ -21,11 +22,23 @@ def least_squares_GD(y, tx, initial_w, max_iters, gamma):
 
 
 def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
-    pass
+    """Linear regression usign Stochastic Gradient Descent."""
+    w = initial_w
+    for n_iter in range(max_iters):
+        for y_batch, tx_batch in helpers.batch_iter(
+                y, tx, batch_size=1, num_batches=1):
+            # compute a stochastic gradient and loss
+            grad, err = gradients.compute_gradient(y_batch, tx_batch, w)
+            loss = costs.mse(err)
+            # update w through the stochastic gradient update
+            w = w - gamma * grad
+        print("SGD({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
+              bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
+    return w, loss
 
 
 def least_squares(y, tx):
-    """calculate the least squares."""
+    """Least squares using normal equations."""
     a = tx.T.dot(tx)
     b = tx.T.dot(y)
 
@@ -35,12 +48,15 @@ def least_squares(y, tx):
 
 
 def ridge_regression(y, tx, lambda_):
+    """Ridge regression using normal equations."""
     pass
 
 
 def logistic_regression(y, tx, initial_w, max_iters, gamma):
+    """Logistic regression using GD or SGD."""
     pass
 
 
 def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma):
+    """Regularised logistic regression using GD or SGD."""
     pass
