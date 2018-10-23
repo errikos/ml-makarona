46b2d6991862ee4ec24590db31e37550fef7c79c
Ergys Dona <errikosd@gmail.com>
2018-10-23 14:55:35 +0200

    Add hessian

--- project1/src/gradients.py
+++ project1/src/gradients.py
@@ -1,7 +1,15 @@
 import numpy as np
-
+from costs import sigmoid
 
 def compute_gradient(y, tx, w):
     err = y - tx.dot(w)
     grad = -tx.T.dot(err) / len(err)
     return grad, err
+
+
+def hessian(w, tx):
+    def diag_form(xn):
+        s = sigmoid(xn.T.dot(w))
+        return s * (1 - s)
+    S = np.diag(np.array([diag_form(sample) for sample in tx]))
+    return tx.T.dot(S).dot(tx)
