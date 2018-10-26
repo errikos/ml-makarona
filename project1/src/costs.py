Fix logistic regression (hessian)

project1/src/costs.py
@@ -15,7 +15,7 @@ def mae(e):
 
 def sigmoid(z):
     z = np.clip(z, -1000, 1000)
-    return 1 / (1 + np.exp(-z))
+    return 1.0 / (1 + np.exp(-z))
 
 
 def compute_mse(y, tx, w):

project1/src/gradients.py
@@ -8,13 +8,11 @@ def mse_gradient(y, tx, w):
 
 
 def hessian(w, tx):
-    def diag_form(xn):
-        s = sigmoid(xn.T.dot(w))
-        return s * (1 - s)
-    S = np.diag(np.array([diag_form(sample) for sample in tx]))
-    return tx.T.dot(S).dot(tx)
+    sigm = sigmoid(tx.dot(w))
+    S = sigm * (1 - sigm)
+    return np.multiply(tx.T, S) @ tx
 
 
 def log_likelihood_gradient(y, tx, w):
-    diff = sigmoid(tx.dot(w)) - y
-    return tx.T.dot(diff)
+    s = sigmoid(tx.dot(w))
+    return tx.T.dot(s - y)

project1/src/implementations.py
@@ -64,7 +64,8 @@ def logistic_regression(y, tx, initial_w, max_iters, gamma):
 
     for n_iter in range(max_iters):
         grad = gradients.log_likelihood_gradient(y, tx, w)  # compute log-likelihood gradient
-        w = w - gamma * np.linalg.inv(gradients.hessian(w, tx)).dot(grad)  # compute new w
+        hessian = gradients.hessian(w, tx)
+        w = w - gamma * np.linalg.solve(hessian, grad)  # compute new w
         loss = costs.compute_log_likelihood_error(y, tx, w)
         print("LOG({bi}/{ti}): loss={l}".format(bi=n_iter, ti=max_iters-1, l=loss))
     

project1/src/util/parsers.py
@@ -34,15 +34,14 @@ def build_poly(x, degree, do_add_bias):
     """
     numOfFeat = x.shape[1]
     richArray = np.zeros((x.shape[0], degree * numOfFeat))
+
     j = 0
-
     for feat in range(0, numOfFeat):
-
         for i in range(1, degree + 1):
             richArray[:, j] = np.power(x[:, feat], i)
             j = j + 1
 
-    if(do_add_bias):
+    if do_add_bias:
         zeroethPower = np.ones((x.shape[0], 1))
         res = np.concatenate((zeroethPower, richArray), axis=1)
     else: