8dad675f6154d70c25ec26b848545f78195afa6d
Ergys Dona <errikosd@gmail.com>
2018-10-29 21:55:21 +0100

    Add --data-path option to run.py

--- project1/src/run.py
+++ project1/src/run.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
+import argparse
+
 import main as m
 import fitters
 
 def main():
+    parser = argparse.ArgumentParser(description='Higgs Boson ML Challenge')
+    parser.add_argument('--data-path', type=str, default=m.DEFAULT_DATA_PATH,
+                        help='Directory containing train.csv and test.csv.')
+    args = parser.parse_args()
+
     fitter = fitters.RidgeFitter(lambda_=0.02, validation_param=0.8)
-    fitter.run(*m._load_data(m.DEFAULT_DATA_PATH))  # load data and run
+    fitter.run(*m._load_data(args.data_path))  # load data and run
 
 if __name__ == '__main__':
     main()
