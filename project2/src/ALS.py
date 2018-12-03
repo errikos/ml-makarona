f4602db76cd6b129c49233d6bd2805b402a61654
konspras <prakonstantinos@gmail.com>
2018-12-03 17:58:13 +0100

    code init

---
+++ project2/src/ALS.py
@@ -0,0 +1,368 @@
+import numpy as np
+import scipy
+import scipy.io
+import scipy.sparse as sp
+import matplotlib.pyplot as plt
+
+
+from helpers import load_data, preprocess_data
+
+path_dataset = "../data/train.csv"
+ratings = load_data(path_dataset)
+print(ratings[:10,:10].todense())
+vait = [1,2,4,7,8]
+vaus = [2,4,6,8,9]
+#rat = ratings[vait][:,vaus]
+rat = ratings[vait]
+print(rat.todense()[:10,:10])
+rat = rat[:,vaus]
+print(rat.todense()[:10,:10])
+
+print(ratings[vait,vaus].shape)
+
+print(ratings[vait,vaus].todense())
+
+
+# ----------------------------------------------------------------------------------------
+
+
+
+from plots import plot_raw_data
+
+num_items_per_user, num_users_per_item = plot_raw_data(ratings)
+
+print("min # of items per user = {}, min # of users per item = {}.".format(
+        min(num_items_per_user), min(num_users_per_item)))
+
+
+# ----------------------------------------------------------------------------------------
+
+
+def split_data(ratings, num_items_per_user, num_users_per_item,
+               min_num_ratings, p_test=0.1):
+    """split the ratings to training data and test data.
+    Args:
+        min_num_ratings: 
+            all users and items we keep must have at least min_num_ratings per user and per item. 
+    """
+    # set seed
+    np.random.seed(988)
+    
+    # select user and item based on the condition.
+    valid_users = np.where(num_items_per_user >= min_num_ratings)[0]
+    valid_items = np.where(num_users_per_item >= min_num_ratings)[0]
+    valid_ratings = ratings[valid_items, :][: , valid_users]  
+    
+    # init
+    num_rows, num_cols = valid_ratings.shape
+    train = sp.lil_matrix((num_rows, num_cols))
+    test = sp.lil_matrix((num_rows, num_cols))
+    
+    print("the shape of original ratings. (# of row, # of col): {}".format(
+        ratings.shape))
+    print("the shape of valid ratings. (# of row, # of col): {}".format(
+        (num_rows, num_cols)))
+
+    nz_items, nz_users = valid_ratings.nonzero()
+    
+    # split the data
+    for user in set(nz_users):
+        # randomly select a subset of ratings
+        row, col = valid_ratings[:, user].nonzero()
+        selects = np.random.choice(row, size=int(len(row) * p_test))
+        residual = list(set(row) - set(selects))
+
+        # add to train set
+        train[residual, user] = valid_ratings[residual, user]
+
+        # add to test set
+        test[selects, user] = valid_ratings[selects, user]
+
+    print("Total number of nonzero elements in origial data:{v}".format(v=ratings.nnz))
+    print("Total number of nonzero elements in train data:{v}".format(v=train.nnz))
+    print("Total number of nonzero elements in test data:{v}".format(v=test.nnz))
+    return valid_ratings, train, test
+
+# ----------------------------------------------------------------------------------------
+
+from plots import plot_train_test_data
+
+valid_ratings, train, test = split_data(
+        ratings, num_items_per_user, num_users_per_item, min_num_ratings=10, p_test=0.1)
+
+#plot_train_test_data(train, test)
+
+# ----------------------------------------------------------------------------------------
+
+
+from helpers import calculate_mse
+
+def baseline_global_mean(train, test):
+    """baseline method: use the global mean."""
+    # find the non zero ratings in the train
+    nonzero_train = train[train.nonzero()]
+
+    # calculate the global mean
+    global_mean_train = nonzero_train.mean()
+
+    # find the non zero ratings in the test
+    nonzero_test = test[test.nonzero()].todense()
+
+    # predict the ratings as global mean
+    mse = calculate_mse(nonzero_test, global_mean_train)
+    rmse = np.sqrt(1.0 * mse / nonzero_test.shape[1])
+    print("test RMSE of baseline using the global mean: {v}.".format(v=rmse))
+
+baseline_global_mean(train, test)
+
+# ----------------------------------------------------------------------------------------
+
+
+def baseline_user_mean(train, test):
+    """baseline method: use the user means as the prediction."""
+    mse = 0
+    num_items, num_users = train.shape
+
+    for user_index in range(num_users):
+        # find the non-zero ratings for each user in the training dataset
+        train_ratings = train[:, user_index]
+        nonzeros_train_ratings = train_ratings[train_ratings.nonzero()]
+        
+        # calculate the mean if the number of elements is not 0
+        if nonzeros_train_ratings.shape[0] != 0:
+            user_train_mean = nonzeros_train_ratings.mean()
+        else:
+            continue
+        
+        # find the non-zero ratings for each user in the test dataset
+        test_ratings = test[:, user_index]
+        nonzeros_test_ratings = test_ratings[test_ratings.nonzero()].todense()
+        
+        # calculate the test error 
+        mse += calculate_mse(nonzeros_test_ratings, user_train_mean)
+    rmse = np.sqrt(1.0 * mse / test.nnz)
+    print("test RMSE of the baseline using the user mean: {v}.".format(v=rmse))
+
+baseline_user_mean(train, test)
+
+
+# ----------------------------------------------------------------------------------------
+
+
+def baseline_item_mean(train, test):
+    """baseline method: use item means as the prediction."""
+    mse = 0
+    num_items, num_users = train.shape
+    
+    for item_index in range(num_items):
+        # find the non-zero ratings for each item in the training dataset
+        train_ratings = train[item_index, :]
+        nonzeros_train_ratings = train_ratings[train_ratings.nonzero()]
+
+        # calculate the mean if the number of elements is not 0
+        if nonzeros_train_ratings.shape[0] != 0:
+            item_train_mean = nonzeros_train_ratings.mean()
+        else:
+            continue
+        
+        # find the non-zero ratings for each movie in the test dataset
+        test_ratings = test[item_index, :]
+        nonzeros_test_ratings = test_ratings[test_ratings.nonzero()].todense()
+        
+        # calculate the test error 
+        mse += calculate_mse(nonzeros_test_ratings, item_train_mean)
+    rmse = np.sqrt(1.0 * mse / test.nnz)
+    print("test RMSE of the baseline using the item mean: {v}.".format(v=rmse))
+    
+baseline_item_mean(train, test)
+
+# ----------------------------------------------------------------------------------------
+
+def init_MF(train, num_features):
+    """init the parameter for matrix factorization."""
+        
+    num_item, num_user = train.get_shape()
+
+    user_features = np.random.rand(num_features, num_user)
+    item_features = np.random.rand(num_features, num_item)
+
+    # start by item features.
+    # return a vector of how many movies each user has rated
+    item_nnz = train.getnnz(axis=1)
+    item_sum = train.sum(axis=1)
+
+    for ind in range(num_item):
+        # each column of item_features is the mean. why for the first line only
+        # it's like it says: for k=0 we have an initialization
+        item_features[0, ind] = item_sum[ind, 0] / item_nnz[ind]
+    return user_features, item_features
+
+# ----------------------------------------------------------------------------------------
+
+def compute_error(data, user_features, item_features, nz):
+    """compute the loss (MSE) of the prediction of nonzero elements."""
+    mse = 0
+    for row, col in nz:
+        item_info = item_features[:, row]
+        user_info = user_features[:, col]
+        mse += (data[row, col] - user_info.T.dot(item_info)) ** 2
+    return np.sqrt(1.0 * mse / len(nz))
+
+
+# ----------------------------------------------------------------------------------------
+
+
+def matrix_factorization_SGD(train, test):
+    """matrix factorization by SGD."""
+    # define parameters
+    gamma = 0.01
+    num_features = 20   # K in the lecture notes
+    lambda_user = 0.1
+    lambda_item = 0.7
+    num_epochs = 20     # number of full passes through the train set
+    errors = [0]
+    
+    # set seed
+    np.random.seed(988)
+
+    # init matrix
+    user_features, item_features = init_MF(train, num_features)
+    
+    # find the non-zero ratings indices 
+    nz_row, nz_col = train.nonzero()
+    nz_train = list(zip(nz_row, nz_col))
+    nz_row, nz_col = test.nonzero()
+    nz_test = list(zip(nz_row, nz_col))
+
+    print("learn the matrix factorization using SGD...")
+    for it in range(num_epochs):        
+        # shuffle the training rating indices
+        np.random.shuffle(nz_train)
+        
+        # decrease step size
+        gamma /= 1.2
+        
+        for d, n in nz_train:
+            # update W_d (item_features[:, d]) and Z_n (user_features[:, n])
+            item_info = item_features[:, d]
+            user_info = user_features[:, n]
+            err = train[d, n] - user_info.T.dot(item_info)
+    
+            # calculate the gradient and update
+            item_features[:, d] += gamma * (err * user_info - lambda_item * item_info)
+            user_features[:, n] += gamma * (err * item_info - lambda_user * user_info)
+
+        rmse = compute_error(train, user_features, item_features, nz_train)
+        print("iter: {}, RMSE on training set: {}.".format(it, rmse))
+        
+        errors.append(rmse)
+
+    # evaluate the test error
+    rmse = compute_error(test, user_features, item_features, nz_test)
+    print("RMSE on test data: {}.".format(rmse))
+
+matrix_factorization_SGD(train, test) 
+
+# ----------------------------------------------------------------------------------------
+
+def update_user_feature(
+        train, item_features, lambda_user,
+        nnz_items_per_user, nz_user_itemindices):
+    """update user feature matrix: Z.
+    how it works: For each user  go and update this user's predictions (Z)"""
+    
+    num_user = nnz_items_per_user.shape[0]
+    num_feature = item_features.shape[0]
+    lambda_I = lambda_user * sp.eye(num_feature)
+    updated_user_features = np.zeros((num_feature, num_user))
+    # Z
+    for user, items in nz_user_itemindices:
+        # extract the columns corresponding to the prediction for given item
+        # "n"xK
+        M = item_features[:, items]
+        #print(M.shape)
+        
+        # update column row of user features
+        # Kx1
+        V = M @ train[items, user]
+        #print(V.shape)
+        A = M @ M.T + nnz_items_per_user[user] * lambda_I
+        X = np.linalg.solve(A, V)
+        #print("AAAA" + str(X.shape))
+        updated_user_features[:, user] = np.copy(X.T)
+    return updated_user_features
+
+def update_item_feature(
+        train, user_features, lambda_item,
+        nnz_users_per_item, nz_item_userindices):
+    """update item feature matrix."""
+    num_item = nnz_users_per_item.shape[0]
+    num_feature = user_features.shape[0]
+    lambda_I = lambda_item * sp.eye(num_feature)
+    updated_item_features = np.zeros((num_feature, num_item))
+
+    for item, users in nz_item_userindices:
+        # extract the columns corresponding to the prediction for given user
+        M = user_features[:, users]
+        V = M @ train[item, users].T
+        A = M @ M.T + nnz_users_per_item[item] * lambda_I
+        X = np.linalg.solve(A, V)
+        updated_item_features[:, item] = np.copy(X.T)
+    return updated_item_features
+
+
+# ----------------------------------------------------------------------------------------
+
+
+from helpers import build_index_groups
+
+
+def ALS(train, test):
+    """Alternating Least Squares (ALS) algorithm."""
+    # define parameters
+    num_features = 20   # K in the lecture notes
+    lambda_user = 0.1
+    lambda_item = 0.7
+    stop_criterion = 1e-4
+    change = 1
+    error_list = [0, 0]
+    
+    # set seed
+    np.random.seed(988)
+
+    # init ALS
+    user_features, item_features = init_MF(train, num_features)
+    
+    # get the number of non-zero ratings for each user and item
+    nnz_items_per_user, nnz_users_per_item = train.getnnz(axis=0), train.getnnz(axis=1)
+    
+    # group the indices by row or column index
+    #print(train.shape)
+    nz_train, nz_item_userindices, nz_user_itemindices = build_index_groups(train)
+    #print(len(nz_train))
+    #print(nz_item_userindices[:10])
+    #print(nz_user_itemindices[:10])
+
+    # run ALS
+    print("\nstart the ALS algorithm...")
+    while change > stop_criterion:
+        # update user feature & item feature
+        user_features = update_user_feature(
+            train, item_features, lambda_user,
+            nnz_items_per_user, nz_user_itemindices)
+        item_features = update_item_feature(
+            train, user_features, lambda_item,
+            nnz_users_per_item, nz_item_userindices)
+
+        error = compute_error(train, user_features, item_features, nz_train)
+        print("RMSE on training set: {}.".format(error))
+        error_list.append(error)
+        change = np.fabs(error_list[-1] - error_list[-2])
+
+    # evaluate the test error
+    nnz_row, nnz_col = test.nonzero()
+    nnz_test = list(zip(nnz_row, nnz_col))
+    rmse = compute_error(test, user_features, item_features, nnz_test)
+    print("test RMSE after running ALS: {v}.".format(v=rmse))
+
+ALS(train, test)    
